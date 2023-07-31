14cae9168dadda3667267c54b256ea934f0249e2
Tomio <94814005+tomio111@users.noreply.github.com>
2023-07-31 04:46:18 -0500

    Update Github.py
    Password Enter Comment

--- Github.py
+++ Github.py
@@ -13,7 +13,7 @@ def main():
     email = driver.find_element("xpath", '//*[@id="login_field"]')
     email.send_keys('Your Email Address')
     password = driver.find_element("xpath", '//*[@id="password"]')
-    password.send_keys('Your Password')
+    password.send_keys('Enter Your Password')
     submit = driver.find_element("xpath", '//*[@name="commit"]')
     submit.click()
     time.sleep(1000)
