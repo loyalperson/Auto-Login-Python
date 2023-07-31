ee19b63c555dc3b78d6c6bd0a7d3627f93faa13f
loyalperson <loyalman00715@outlook.com>
2023-07-31 03:54:41 -0500

    complete

---
+++ Github.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+def main():
+    service = Service(executable_path="./chromedriver")
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get('https://github.com/login')
+    email = driver.find_element("xpath", '//*[@id="login_field"]')
+    email.send_keys('Your Email Address')
+    password = driver.find_element("xpath", '//*[@id="password"]')
+    password.send_keys('Your Password')
+    submit = driver.find_element("xpath", '//*[@name="commit"]')
+    submit.click()
+    time.sleep(1000)
+if __name__ == '__main__':
+    main()
